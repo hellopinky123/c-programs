fffb29592203f96deb515abb8c3d9bb1aa489a30
kalpana dash <dashk8658@gmail.com>
2022-12-25 23:27:15 +0530

    this is my 11th commit

--- program10.c
+++ program10.c
@@ -4,7 +4,7 @@ int main() {//conversion of f to c
 float c,F;
 printf("enter temp in fahreneit");
 scanf("%f", &F);
-c=(F-32) *5/9;
+c=(F-32)*5/9;
 printf("%f fahreneit=%f celcius", F,c);
 
 
---
+++ program11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {//conversion of c to f
+float c,f;
+printf("enter temp in celcius");
+scanf("%f", &c);
+f=(c*9/5)+32;
+printf("%f celcius=%f fahreneit", c,f);
+
+
+
+
+}
