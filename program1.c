f272c359f6121fcb1be30c09abca28e2483dd4c6
kalpana dash <dashk8658@gmail.com>
2022-12-25 20:41:00 +0530

    this is my 1st commit

---
+++ program1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+
+
+int a,b,sum,sub,mul,div;
+printf("enter a :");
+scanf("%d", &a);
+printf("enter b");
+scanf("%d", &b);
+sum=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+printf("%d\n%d\n%d\n%d\n",sum,sub,mul,div);
+return 0;
+}
+
+
+
