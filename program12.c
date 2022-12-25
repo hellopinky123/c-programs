fbcceb6700818f1997f74e1b52700b5999523c44
kalpana dash <dashk8658@gmail.com>
2022-12-25 23:33:54 +0530

    this is my 12th commit

---
+++ program12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+
+    int n;
+    printf("enter n :");
+    scanf("%d", &n);
+    if(n%2==0) {
+        printf("no is even");
+    } else {
+        printf("odd");
+    }
+
+
+}
