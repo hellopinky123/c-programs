467bdd82947c92ab1fb4354ba41336931a803493
kalpana dash <dashk8658@gmail.com>
2022-12-25 23:43:34 +0530

    this is my 13th commit

---
+++ program13.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {//largest no among two nos
+
+    int num1;
+    int num2;
+    printf("enter num1 :");
+    scanf("%d", &num1);
+    printf("enter num2");
+    scanf("%d", &num2);
+    if(num1>num2) {
+        printf("num1 is maximum");
+    }
+    if(num2>num1) {
+        printf("num2 is maximum");
+    }
+
+
+    
+}
