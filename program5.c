edd31ee1b0243116c4176200f1da502fd98b0827
kalpana dash <dashk8658@gmail.com>
2022-12-25 21:13:57 +0530

    this is my 6th commit

---
+++ program5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {//calculation of simple intrest
+int p;
+int t;
+int r;
+int si;
+printf("enter p");
+scanf("%d",&p);
+printf("enter t");
+scanf("%d", &t);
+printf("enter r");
+scanf("%d", &r);
+si=(p*t*r)/100;
+printf("%d", si);
+return 0;
+
+
+
+
+
+}
