64b294b305ad656abce0d38b78216684a69de693
kalpana dash <dashk8658@gmail.com>
2022-12-25 22:55:42 +0530

    this is my 7th commit

--- program6.c
+++ program6.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-int main() {//area of circle
-float r;
+int main() {//area of square
+int r;
 printf("enter r");
-scanf("%f",&r);
+scanf("%d",&r);
 
-int area=3.14*r*r;
-printf("%f", area);
+int area=r*r;
+printf("%d", area);
 
 
 
