908fc6a72413315621b05a06374c1710d637895a
kalpana dash <dashk8658@gmail.com>
2022-12-28 00:23:08 +0530

    this is my 30th commit

--- program30.c
+++ program30.c
@@ -2,15 +2,17 @@
 int main() {
 
     int i=1,n,fact=1;
-    printf("enter the number :");
+    printf("enter any number :");
     scanf("%d", &n);
+
     while(i<=n) {
         fact=fact*i;
         printf("%d\n",i);
         i++;
-    } 
-    printf("%d", fact);
-    
 
-    
+    }
+    printf("%d\n", fact);
+
+
+    return 0;
 }
