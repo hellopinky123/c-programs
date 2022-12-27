cd345c349fbf6e04f7e956f410fd24bd02735f3b
kalpana dash <dashk8658@gmail.com>
2022-12-27 19:30:05 +0530

    this is my 26th commit

---
+++ program26.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+
+
+    int i=0,n;
+    printf("enter the number :");
+    scanf("%d", &n);
+    int sum=0;
+    while(i<=n) {
+        sum=sum+i;
+        printf("%d\n", i);
+        i++;
+    }
+    printf("%d\n", sum);
+    }
+
